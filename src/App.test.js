52bee4053988ddbfb560bbfb375421bb2bc73d6a
Mark Rivera <Mriver1991@gmail.com>
2020-05-14 22:04:30 -0400

    started project

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,33 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+
+test("Assure that all inputs are visible in document", async () => {
+  const { findByLabelText } = render(<App />);
+
+  // First Name Check
+  const nameInput = await findByLabelText(/First Name/i);
+  expect(nameInput).toBeInTheDocument();
+  expect(nameInput).toBeVisible();
+
+  // Last Name Check
+  const lastNameInput = await findByLabelText(/Last Name/i);
+  expect(lastNameInput).toBeInTheDocument();
+  expect(lastNameInput).toBeVisible();
+
+  // Email Check
+  const emailInput = await findByLabelText(/Email/i);
+  expect(emailInput).toBeInTheDocument();
+  expect(emailInput).toBeVisible();
+
+  // Message Box Check
+  const msgInput = await findByLabelText(/Message/i);
+  expect(msgInput).toBeInTheDocument();
+  expect(msgInput).toBeVisible();
+
 });
